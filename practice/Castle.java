23863e89f9febcdb534e3aa34d59967f09f71ad0
chrisli3064 <chrisli3064@users.noreply.github.com>
2018-09-10 09:07:36 -0500

    made castle

---
+++ practice/Castle.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Castle{
+  public static void main(String args[])throws IOException{
+	  BufferedReader br = new BufferedReader(new FileReader("castle.in"));
+	  StringTokenizer st = new StringTokenizer(br.readLine());
+	  int columns = Integer.valueOf(st.nextToken());
+	  int rows = Integer.valueOf(st.nextToken());
+	  int[][] castle = new int[rows][columns];
+	  final int w = 1;
+	  final int n = 2;
+	  final int e = 4;
+	  final int s = 8;
+	  for(int i=0; i<rows; i++){
+		  StringTokenizer st1 = new StringTokenizer(br.readLine());
+		  for(int j=0; j<columns; j++){
+			  castle[i][j]=Integer.valueOf(st.nextToken());
+		  }
+	  }
+  }
+}
+class node{
+  int total;
+  boolean north;
+  boolean south;
+  boolean west;
+  boolean east;
+
+  public node(int total, boolean north, boolean south, boolean west, boolean east){
+        this.total = total;
+	this.north = north;
+	this.south = south;
+	this.west = west;
+	this.east = east;
+  }
+
+  public int getTotal(){
+	  return total;
+  }
+  public boolean getNorth(){
+	  return north;
+  }
+  public boolean getWest(){
+	  return west;
+  }
+  public boolean getEast(){
+	  return east;
+  }
+  public boolean getSouth(){
+	  return south; 
+  }
+}
