82fab58a6f1104a5148d514069a4708c9ce1f403
chrisli3064 <chrisli3064@users.noreply.github.com>
2018-11-19 09:33:51 -0600

    algo

---
+++ practice/frac1.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class frac1
+{
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new FileReader("frac1.in"));
+        PrintWriter pw = new PrintWriter(new FileWriter("frac1.out"));
+        int n = Integer.valueOf(br.readLine());
+        System.out.println("0/1");
+        for(int i=1; i<n; i++){
+            
+        }
+    }
+}
