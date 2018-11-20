ef10b8cd7883668284a1fcea1f2fdeb6a52a5948
chrisli3064 <chrisli3064@users.noreply.github.com>
2018-11-20 09:18:49 -0600

    ..

--- practice/frac1.java
+++ practice/frac1.java
@@ -1,14 +1,34 @@
-import java.io.*;
 import java.util.*;
-public class frac1
-{
-    public static void main(String[] args)throws IOException{
-        BufferedReader br = new BufferedReader(new FileReader("frac1.in"));
-        PrintWriter pw = new PrintWriter(new FileWriter("frac1.out"));
-        int n = Integer.valueOf(br.readLine());
-        System.out.println("0/1");
-        for(int i=1; i<n; i++){
-            
-        }
-    }
+import java.io.*;
+/**
+*PROB: frac1
+*NAME: christo57
+**/
+class frac1{
+
+   public static void main(String[] args)throws IOException{
+       BufferedReader br = new BufferedReader(new FileReader("frac1.in"));
+       PrintWriter pw = new PrintWriter(new FileWriter("frac1.out"));
+       
+       int n = Integer.valueOf(br.readLine());
+       
+       HashMap<Double, String> hm = new HashMap<Double,String>();
+       HashSet<Double> hs = new HashSet<Double>();
+       System.out.println("0/1");
+       for(int i=1; i<n; i++){
+           for(int j=1; j<i; j++){
+               hm.put(i/Double.valueOf(j), i+"/"+j);
+               hs.add(i/Double.valueOf(j));
+           }
+       }
+       ArrayList<Double> al = new ArrayList<Double>();
+       Collections.sort(al); 
+       for(int i=0; i<al.size(); i++){
+           System.out.print(al.get(i)+ " ");
+           pw.write(hm.get(al.get(i)));
+       }
+       pw.close();
+   }
+
+
 }
